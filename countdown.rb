337408fa21ac20cae3fcab78e3760b72ff85a7c2
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-27 22:13:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown（number)
+  while number>0 
+  puts "#{number} SECONDS!"
+  number-=number
+  break if number==0 
+end
+return "HAPPY NEW YEAR!"
 end
